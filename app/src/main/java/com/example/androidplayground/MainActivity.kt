50a6d4b645f4de0c2760918558cf4ba2e2518052
谢俊彦 <junyan.xie@hollyland-tech.com>
2023-04-07 18:24:09 +0800

    测试arrow

--- app/src/main/java/com/example/androidplayground/MainActivity.kt
+++ app/src/main/java/com/example/androidplayground/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.androidplayground
 
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,6 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.raise.Raise
+import arrow.core.right
 import com.example.androidplayground.ui.theme.AndroidPlaygroundTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -42,4 +48,29 @@ fun DefaultPreview() {
     AndroidPlaygroundTheme {
         Greeting("Android")
     }
+}
+
+object UserNotFound
+data class User(val id: Long)
+
+val user: Either<UserNotFound, User> = User(1).right()
+
+
+fun main() {
+    user.fold(
+        { println("User not found") },
+        { println("User found: $it") }
+    )
+
+    val a: A<Image, ImageView> = B<Image>()
+}
+
+open class A <out L, out R> {
+
+
+}
+
+class B <out T>: A<T, Nothing>() {
+
+
 }
